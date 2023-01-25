487d47e41c629019ced675e679e3d2a6a374ceef
igneefleur <57841033+igneefleur@users.noreply.github.com>
2023-01-25 13:35:04 -0500

    change to third person blueprint version template (instead of C++ version)

--- Source/GGJ_2023/GGJ_2023.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GGJ_2023.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GGJ_2023, "GGJ_2023" );
- 
